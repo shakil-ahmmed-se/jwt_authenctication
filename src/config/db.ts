1ba4efd211e91efedfd089abfe31608f261caa8e
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-11-23 22:10:56 +0600

    middlware

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,10 @@ import mongoose from "mongoose"
 import { MONGO_URI } from "../constants/env";
 
 
-const connectToDatabase = async () => {
+export const connectToDatabase = async () => {
     try {
-        await mongoose.connect(MONGO_URI)
+        await mongoose.connect(MONGO_URI);
+        console.log("Successfully connect to DB")
     } catch (error) {
         console.log('Could not connected to database', error);
         process.exit(1);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
+import { connectToDatabase } from "./config/db";
+import { APP_ORIGIN, NODE_ENV, PORT } from "./constants/env";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
-app.get("/", (req:Request, res:Response)=>{
-     res.status(200).json({status:"HHELLO WORKD"});
-})
+app.use(express.json());
 
-app.listen(4004, ()=>{
-    console.log(`Server is running on port 4004 development environment`)
-})
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+    throw new Error("This is a test Error")
+  res.status(200).json({ status: "HHELLO WORKD" });
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT} ${NODE_ENV} environment`);
+  await connectToDatabase();
+});
